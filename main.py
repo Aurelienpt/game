2b06642f5dfb14745971f5086b8301953a0c52be
Aurelienpt <aurelpt@gmail.com>
2017-11-09 11:37:44 +0100

    Beta

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ print("Game start")
 nombre = randrange(1, 100)
 i = 0
 while i != 7:
-	proposition = input("Try to find the good number between 1 and 100 : ")
-	if proposition == nombre:
-		print("You just win ! Answer :", nombre)
-		exit()
-	elif proposition > nombre:
-		print(proposition, ": Smaller !")
+	proposition = raw_input("Try to find the good number between 1 and 100 : ")
+	try:
+		proposition = int(proposition)
+	except ValueError:
+			print(str(proposition) + " is not a valid number")
+			continue
+	if isinstance(proposition, int):
+		if proposition == nombre:
+			print("You just win ! Answer :", nombre)
+			exit()
+		elif proposition > nombre:
+			print(str(proposition) + " : Smaller !")
+		else:
+			print(str(proposition) + " : Higher !")
+		i += 1
 	else:
-		print(proposition, ": Higher !")
-	i += 1
+		print(str(proposition) + " is not a valid number")
 print("End of the game, you lose :(")
